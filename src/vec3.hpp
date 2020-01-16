9d7e3e9cc81966ee9a3cd8ad741aeef4f67be591
evopen <520dhh@gmail.com>
2020-01-16 23:39:08 +0800

    fix bound checking and naming

--- src/vec3.hpp
+++ src/vec3.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <array>
 #include <cmath>
+#include <gsl/gsl>
 #include <iostream>
 
 class Vec3
@@ -8,20 +10,21 @@ class Vec3
 public:
     Vec3() = default;
 
-    Vec3(float e0, float e1, float e2) : e{e0, e1, e2} {}
+    Vec3(float e0, float e1, float e2) : e_{e0, e1, e2} {}
 
-    [[nodiscard]] inline auto X() const -> float { return e[0]; }
-    [[nodiscard]] inline auto Y() const -> float { return e[1]; }
-    [[nodiscard]] inline auto Z() const -> float { return e[2]; }
-    [[nodiscard]] inline auto R() const -> float { return e[0]; }
-    [[nodiscard]] inline auto G() const -> float { return e[1]; }
-    [[nodiscard]] inline auto B() const -> float { return e[2]; }
+    [[nodiscard]] inline auto X() const -> float { return e_[0]; }
+    [[nodiscard]] inline auto Y() const -> float { return e_[1]; }
+    [[nodiscard]] inline auto Z() const -> float { return e_[2]; }
+    [[nodiscard]] inline auto R() const -> float { return e_[0]; }
+    [[nodiscard]] inline auto G() const -> float { return e_[1]; }
+    [[nodiscard]] inline auto B() const -> float { return e_[2]; }
 
-    inline auto operator[](const int kI) const -> float { return e[kI]; }
-    inline auto operator[](const int kI) -> float& { return e[kI]; }
+    inline auto operator[](const int kI) const -> float { return gsl::at(e_, kI); }
+    inline auto operator[](const int kI) -> float& { return gsl::at(e_, kI); }
 
     [[nodiscard]] inline auto Length() const -> float { return sqrt(SquareLength()); }
-    [[nodiscard]] inline auto SquareLength() const -> float { return e[0] * e[0] + e[1] * e[1] + e[2] * e[2]; }
+    [[nodiscard]] inline auto SquareLength() const -> float { return e_[0] * e_[0] + e_[1] * e_[1] + e_[2] * e_[2]; }
 
-    float e[3]{};
+private:
+    std::array<float, 3> e_;
 };
