53611982175c9b26592161ccb033334e33253b11
evopen <520dhh@gmail.com>
2020-01-16 20:25:19 +0800

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,48 @@
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+
+auto main() noexcept -> int
+{
+    try
+    {
+        std::cout << std::filesystem::current_path();
+        std::ofstream fs("result.ppm", std::fstream::out);
+        if (!fs)
+        {
+            throw std::runtime_error("failed to open file");
+        }
+
+        const int kNx          = 200;  // width
+        const int kNy          = 100;  // height
+        const int kMaxColor    = 255;
+        const float kBlueValue = 0.2F;
+
+        fs << "P3\n" << kNx << ' ' << kNy << "\n255\n";
+        for (int i = 0; i < kNy; i++)  // for every row
+        {
+            for (int j = 0; j < kNx; j++)  // for every column
+            {
+                auto r  = static_cast<float>(j) / kNx;
+                auto g  = static_cast<float>(i) / kNy;
+                float b = kBlueValue;
+                int ir  = static_cast<int>(r * kMaxColor);
+                int ig  = static_cast<int>(g * kMaxColor);
+                int ib  = static_cast<int>(b * kMaxColor);
+                fs << ir << ' ' << ig << ' ' << ' ' << ib << "\t";
+            }
+            fs << "\n";
+        }
+        fs.close();
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    catch (...)
+    {
+        std::cerr << "Unknown exception" << std::endl;
+    }
+
+    return 0;
+}
