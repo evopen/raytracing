d9502a289da8c02acb06f94fbf3d0ee55b91ad1a
evopen <520dhh@gmail.com>
2020-01-17 15:31:07 +0800

    add more methods for Vec3 class

--- src/vec3.hpp
+++ src/vec3.hpp
@@ -22,9 +22,109 @@ public:
     inline auto operator[](const int kI) const -> float { return gsl::at(e_, kI); }
     inline auto operator[](const int kI) -> float& { return gsl::at(e_, kI); }
 
+    inline Vec3& operator+=(Vec3& v)
+    {
+        e_[0] += v[0];
+        e_[1] += v[1];
+        e_[2] += v[2];
+        return *this;
+    }
+
+    inline Vec3& operator-=(Vec3& v)
+    {
+        e_[0] -= v[0];
+        e_[1] -= v[1];
+        e_[2] -= v[2];
+        return *this;
+    }
+
+    inline Vec3& operator*=(Vec3& v)
+    {
+        e_[0] *= v[0];
+        e_[1] *= v[1];
+        e_[2] *= v[2];
+        return *this;
+    }
+
+    inline Vec3& operator*=(float t)
+    {
+        e_[0] *= t;
+        e_[1] *= t;
+        e_[2] *= t;
+        return *this;
+    }
+
+    inline Vec3& operator/=(Vec3& v)
+    {
+        e_[0] /= v[0];
+        e_[1] /= v[1];
+        e_[2] /= v[2];
+        return *this;
+    }
+
+    inline Vec3& operator/=(float t)
+    {
+        float k = 1 / t;
+        e_[0] *= t;
+        e_[1] *= t;
+        e_[2] *= t;
+        return *this;
+    }
+
     [[nodiscard]] inline auto Length() const -> float { return sqrt(SquareLength()); }
     [[nodiscard]] inline auto SquareLength() const -> float { return e_[0] * e_[0] + e_[1] * e_[1] + e_[2] * e_[2]; }
 
 private:
     std::array<float, 3> e_;
 };
+
+inline Vec3 operator+(const Vec3& v1, const Vec3& v2)
+{
+    return Vec3(v1[0] + v2[0], v1[1] + v2[1], v1[2] + v2[2]);
+}
+
+inline Vec3 operator-(const Vec3& v1, const Vec3& v2)
+{
+    return Vec3(v1[0] - v2[0], v1[1] - v2[1], v1[2] - v2[2]);
+}
+
+inline Vec3 operator*(const Vec3& v1, const Vec3& v2)
+{
+    return Vec3(v1[0] * v2[0], v1[1] * v2[1], v1[2] * v2[2]);
+}
+
+inline Vec3 operator*(float t, const Vec3& v)
+{
+    return Vec3(t * v[0], t * v[1], t * v[2]);
+}
+
+inline Vec3 operator*(const Vec3& v, float t)
+{
+    return Vec3(t * v[0], t * v[1], t * v[2]);
+}
+
+inline Vec3 operator/(const Vec3& v1, const Vec3& v2)
+{
+    return Vec3(v1[0] / v2[0], v1[1] / v2[1], v1[2] / v2[2]);
+}
+
+inline Vec3 operator/(const Vec3& v, const float t)
+{
+    return Vec3(v[0] / t, v[1] / t, v[2] / t);
+}
+
+
+inline float dot(const Vec3& v1, const Vec3& v2)
+{
+    return v1[0] * v2[0] + v1[1] * v2[1] + v1[2] + v2[2];
+}
+
+inline Vec3 cross(const Vec3& v1, const Vec3& v2)
+{
+    return Vec3(v1[1] * v2[2] - v1[2] * v2[1], v1[2] * v2[0] - v1[0] * v2[2], v1[0] * v2[1] - v1[1] * v2[0]);
+}
+
+inline Vec3 UnitVector(const Vec3& v)
+{
+    return v / v.Length();
+}
--- test/vec3_test.cpp
+++ test/vec3_test.cpp
@@ -4,7 +4,7 @@
 
 TEST(Vec3Test, ConstructorAndGetter)
 {
-    Vec3 v0(2.2, 3.8, 44.3423);
+    Vec3 v0(2.2F, 3.8F, 44.3423F);
     EXPECT_NEAR(v0.R(), 2.2, 0.001);
     EXPECT_NEAR(v0.G(), 3.8, 0.001);
     EXPECT_NEAR(v0.B(), 44.3423, 0.001);
@@ -16,4 +16,69 @@ TEST(Vec3Test, ConstructorAndGetter)
     EXPECT_NEAR(v0[2], 44.3423, 0.001);
     EXPECT_NEAR(v0.Length(), 44.5592, 0.001);
     EXPECT_NEAR(v0.SquareLength(), 1985.52230464, 0.1);
+}
+
+class Vec3OperatorTest : public ::testing::Test
+{
+protected:
+    void SetUp() override
+    {
+        v1 = Vec3(2.3F, 3.8F, 4.4F);
+        v2 = Vec3(34.5F, 234.F, 53.9F);
+    }
+
+    Vec3 v1, v2;
+};
+
+TEST_F(Vec3OperatorTest, Operator)
+{
+    Vec3 v0 = v1 + v2;
+    EXPECT_NEAR(v0[0], 36.8, 0.001);
+    EXPECT_NEAR(v0[1], 237.8, 0.001);
+    EXPECT_NEAR(v0[2], 58.3, 0.001);
+    v0 = v2 - v1;
+    EXPECT_NEAR(v0[0], 32.2, 0.001);
+    EXPECT_NEAR(v0[1], 230.2, 0.001);
+    EXPECT_NEAR(v0[2], 49.5, 0.001);
+}
+
+TEST_F(Vec3OperatorTest, CompoundOperatorAdd)
+{
+    v1 += v2;
+    EXPECT_NEAR(v1[0], 36.8, 0.001);
+    EXPECT_NEAR(v1[1], 237.8, 0.001);
+    EXPECT_NEAR(v1[2], 58.3, 0.001);
+}
+
+TEST_F(Vec3OperatorTest, CompoundOperatorSubtract)
+{
+    v1 -= v2;
+    EXPECT_NEAR(v1[0], -32.2, 0.001);
+    EXPECT_NEAR(v1[1], -230.2, 0.001);
+    EXPECT_NEAR(v1[2], -49.5, 0.001);
+}
+
+TEST_F(Vec3OperatorTest, CompoundOperatorMultiplication)
+{
+    v1 *= v2;
+    EXPECT_NEAR(v1[0], 79.35, 0.1);
+    EXPECT_NEAR(v1[1], 889.2, 0.1);
+    EXPECT_NEAR(v1[2], 237.16, 0.1);
+}
+
+TEST_F(Vec3OperatorTest, CompoundOperatorDivision)
+{
+    v1 /= v2;
+    EXPECT_NEAR(v1[0], 0.06666, 0.1);
+    EXPECT_NEAR(v1[1], 0.016239, 0.1);
+    EXPECT_NEAR(v1[2], 0.08163, 0.1);
+}
+
+TEST(Vec3Test, UnitVector)
+{
+    Vec3 v(2, 3, 4);
+    Vec3 unit_v = UnitVector(v);
+    EXPECT_NEAR(unit_v[0], 0.424264, 0.1);
+    EXPECT_NEAR(unit_v[1], 0.565685, 0.1);
+    EXPECT_NEAR(unit_v[2], 0.707107, 0.1);
 }
