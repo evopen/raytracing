8356698613c4c37e800e7bc93b9d65f1441b8d33
evopen <520dhh@gmail.com>
2020-01-18 20:09:42 +0800

    Hittables list class for finding closest hit point in list

---
+++ src/hittable_list.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "hittable.h"
+
+#include <vector>
+
+class HittableList : public Hittable
+{
+public:
+    HittableList() = default;
+    HittableList(const std::vector<Hittable*>* list) : list_(list) {}
+
+    auto Hit(const Ray& ray, float t_min, float t_max, HitRecord& rec) const -> bool;
+    auto List() -> const std::vector<Hittable*>* { return list_; }
+
+private:
+    const std::vector<Hittable*>* list_;
+};
