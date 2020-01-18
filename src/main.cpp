2e7f84973afb63fdf85898842e4a8e3c7ac3c176
evopen <520dhh@gmail.com>
2020-01-18 20:14:32 +0800

    render picture with two spheres
    render progress can be viewed line by line

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
+#include "hittable.h"
+#include "hittable_list.h"
 #include "ray.h"
+#include "sphere.h"
 #include "vec3.h"
 
 #include <filesystem>
@@ -6,35 +9,13 @@
 #include <iostream>
 
 
-// return the coefficient of ray direction to the hit point
-auto HitSphere(const Vec3& center, const float kRadius, const Ray& ray) -> float
+auto Color(const Ray& ray, Hittable* hittable) -> Vec3
 {
-    float a = Dot(ray.Direction(), ray.Direction());
-    float b = 2 * Dot(ray.Origin() - center, ray.Direction());
-    float c = Dot(ray.Origin() - center, ray.Origin() - center) - kRadius * kRadius;
+    HitRecord rec;
 
-    float discriminant = b * b - 4 * a * c;
-
-    if (discriminant < 0)
+    if (hittable->Hit(ray, 0, FLT_MAX, rec))
     {
-        return -1;
-    }
-    else  // solve t in quadratic equation using quadratic formula
-    {
-        return (-b - sqrt(discriminant)) / (2 * a);
-    }
-}
-
-
-auto Color(const Ray& ray) -> Vec3
-{
-    const Vec3 kSphereCenter  = Vec3(0, 0, -1);
-    const float kSphereRadius = 0.5;
-    float t                   = HitSphere(kSphereCenter, kSphereRadius, ray);
-    if (t > 0)
-    {
-        Vec3 normal = UnitVector(ray.PointAtParameter(t) - kSphereCenter);
-        return 0.5 * (normal + 1.f);
+        return 0.5 * (rec.normal + 1);
     }
     Vec3 direction    = UnitVector(ray.Direction());
     float color_ratio = 0.5 * (direction.Y() + 1.f);
@@ -55,27 +36,51 @@ auto main() noexcept -> int
         {
             throw std::runtime_error("failed to open file");
         }
+        fs.fill('0');
 
-        const int kNx          = 200;  // width
-        const int kNy          = 100;  // height
-        const int kMaxColor    = 255;
-        const float kBlueValue = 0.2F;
+        const int kNx       = 200;  // width
+        const int kNy       = 100;  // height
+        const int kMaxColor = 255;
 
-        fs << "P3\n" << kNx << ' ' << kNy << "\n255\n";
+        fs << "P3\n" << kNx << ' ' << kNy << "\n255\n" << std::flush;
+        int pos = fs.tellp();
+
+        // initialize image with black
         for (int i = 0; i < kNy; i++)  // for every row
+        {
+            for (int j = 0; j < kNx; j++)  // for every column
+            {
+                fs << std::setw(3) << 0 << ' ' << std::setw(3) << 0 << ' ' << std::setw(3) << 0 << "\t";
+            }
+            fs << "\n";
+        }
+        fs.seekp(pos);
+        fs.flush();
+
+
+        Sphere small_sphere(Vec3(0, 0, -1), 0.5);
+        Sphere big_sphere(Vec3(0, -100.5, -1), 100);
+
+        std::vector<Hittable*> hittables;
+        hittables.push_back(&small_sphere);
+        hittables.push_back(&big_sphere);
+
+        HittableList world(&hittables);
+
+        for (int i = kNy - 1; i >= 0; i--)  // for every row
         {
             for (int j = 0; j < kNx; j++)  // for every column
             {
                 float u = (float) j / kNx;
                 float v = (float) i / kNy;
                 Ray r(origin, Vec3(lower_left_corner + u * horizontal + v * vertical));
-                Vec3 col = Color(r);
+                Vec3 col = Color(r, &world);
                 int ir   = static_cast<int>(col.R() * kMaxColor);
                 int ig   = static_cast<int>(col.G() * kMaxColor);
                 int ib   = static_cast<int>(col.B() * kMaxColor);
-                fs << ir << ' ' << ig << ' ' << ' ' << ib << "\t";
+                fs << std::setw(3) << ir << ' ' << std::setw(3) << ig << ' ' << std::setw(3) << ib << "\t";
             }
-            fs << "\n";
+            fs << "\n" << std::flush;
         }
         fs.close();
     }
