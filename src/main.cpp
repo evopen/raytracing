fdc8a4de340de7506a7ccbe02b5a3b944064dfb8
evopen <520dhh@gmail.com>
2020-01-19 23:07:40 +0800

    implement diffuse reflection

--- src/main.cpp
+++ src/main.cpp
@@ -12,18 +12,25 @@
 #include <iostream>
 #include <limits>
 
+auto RandomInUnitSphere() -> Vec3
+{
+    Vec3 random_vec(rt::random::RandomReal(-1., 1.), rt::random::RandomReal(-1., 1.), rt::random::RandomReal(-1., 1.));
+    return UnitVector(random_vec);
+}
 
 auto Color(const Ray& ray, Hittable* hittable) -> Vec3
 {
     HitRecord rec;
 
-    if (hittable->Hit(ray, 0, std::numeric_limits<float>::max(), rec))
+    if (hittable->Hit(ray, 0.001, std::numeric_limits<float>::max(), rec))
     {
-        return 0.5 * (rec.normal + 1);
+        Vec3 diffuse_direction = rec.p + rec.normal + RandomInUnitSphere() - rec.p;
+        Ray diffuse_ray(rec.p, diffuse_direction);
+        return 0.5 * Color(diffuse_ray, hittable);
     }
     Vec3 direction    = UnitVector(ray.Direction());
-    float color_ratio = 0.5 * (direction.Y() + 1.f);
-    return color_ratio * Vec3(1, 0.2, 0.2) + (1 - color_ratio) * Vec3(1, 1, 1);
+    float color_ratio = 0.5 * (direction.Y() + 1.F);
+    return color_ratio * Vec3(0.2, 0.2, 1) + (1 - color_ratio) * Vec3(1, 1, 1);
 }
 
 auto main() noexcept -> int
@@ -42,9 +49,9 @@ auto main() noexcept -> int
         }
         fs.fill('0');
 
-        const int kNx       = 200;  // width
-        const int kNy       = 100;  // height
-        const int kSamples  = 100;  // samples count
+        const int kNx       = 800;  // width
+        const int kNy       = 400;  // height
+        const int kSamples  = 32;  // samples count
         const int kMaxColor = 255;
 
         fs << "P3\n" << kNx << ' ' << kNy << "\n255\n" << std::flush;
@@ -74,6 +81,8 @@ auto main() noexcept -> int
 
         Camera cam;
 
+        float progress = 0;
+
         for (int i = kNy - 1; i >= 0; i--)  // for every row
         {
             for (int j = 0; j < kNx; j++)  // for every column
@@ -88,13 +97,18 @@ auto main() noexcept -> int
                     col += Color(r, &world);
                 }
                 col /= kSamples;
-
+                col = Vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
                 int ir = static_cast<int>(col.R() * kMaxColor);
                 int ig = static_cast<int>(col.G() * kMaxColor);
                 int ib = static_cast<int>(col.B() * kMaxColor);
                 fs << std::setw(3) << ir << ' ' << std::setw(3) << ig << ' ' << std::setw(3) << ib << "\t";
             }
             fs << "\n" << std::flush;
+            if ((float) (kNy - i) / kNy > (progress + 0.05))
+            {
+                progress += 0.05;
+                std::cout << progress * 100 << '%' << std::endl;
+            }
         }
         fs.close();
     }
